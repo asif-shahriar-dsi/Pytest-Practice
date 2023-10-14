af8a804b9c12cbd5ba2863647b07a1266233d751
asif-shahriar-dsi <asif.shahriar@dsinnovators.com>
2023-10-14 13:58:31 +0600

    initial push

---
+++ Basic/Cons.py
@@ -0,0 +1,17 @@
+
+
+
+class Plane:
+    def __init__(self):
+        pass
+
+
+class Car:
+    pass
+
+
+class Train:
+    def __init__(self, length, number_of_passenger):
+        self.length = length
+        self.number = number_of_passenger
+
